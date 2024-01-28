d3cf85a6a599a9e9d1fa6d1affe341c8b31615e0
sundewang <631460660@qq.com>
2024-01-28 22:03:04 +0800

    添加日志

--- web-demo/src/main/java/xyz/sun/web/controller/GreetingController.java
+++ web-demo/src/main/java/xyz/sun/web/controller/GreetingController.java
@@ -1,5 +1,7 @@
 package xyz.sun.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +15,23 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 @RestController
 public class GreetingController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GreetingController.class);
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/log")
+    public String log(@RequestParam(value = "count") int count) {
+        for (int i = 0; i < count; i++) {
+            LOGGER.info("this is {} log", i);
+            LOGGER.warn("this is {} warn log", i);
+            LOGGER.error("this is {} error log", i);
+        }
+        return String.valueOf(count);
     }
 }
